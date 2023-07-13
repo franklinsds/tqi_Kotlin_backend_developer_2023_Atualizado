3ab6fdb0f1a316d6d6cf3d36ee0a9cde6c91fd06
frankin <franklinsds@gmail.com>
2023-07-13 10:30:09 -0400

    Implementação das classes do desafio auto atendimento da empresa fictícia JuMarket

---
+++ src/main/kotlin/com/JuMarket/autoatendimento/system/AutoatendimentoSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.JuMarket.autoatendimento.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutoatendimentoSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<AutoatendimentoSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/JuMarket/autoatendimento/system/AutoatendimentoSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.JuMarket.autoatendimento.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutoatendimentoSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
